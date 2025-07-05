fabbb575c5f93a09f67fb4c12bfaa76924099ffe
Shrish Mohapatra <shrish.mohapatra@agilitypr.com>
2025-07-05 14:16:11 -0400

    create chat component

--- website/src/repl/components/panel/WelcomeMuser.jsx
+++ website/src/repl/components/panel/WelcomeMuser.jsx
@@ -1,17 +1,15 @@
 import { useSettings } from '@src/settings.mjs';
+import { ChatWindow } from '@src/repl/components/chat/ChatWindow.jsx';
 
 const { BASE_URL } = import.meta.env;
 const baseNoTrailing = BASE_URL.endsWith('/') ? BASE_URL.slice(0, -1) : BASE_URL;
 
 export function WelcomeMuser({ context }) {
   const { fontFamily } = useSettings();
+
   return (
-    <div className="prose dark:prose-invert min-w-full pt-2 font-sans pb-8 px-4 " style={{ fontFamily }}>
-      <h3>welcome to muser</h3>
-      <p>
-        You have found <span className="underline">muser</span>, a new vibe coding music platform to generate music.
-        Chat to get started.
-      </p>
+    <div className="prose dark:prose-invert min-w-full pt-2 font-sans pb-8 px-4 h-full" style={{ fontFamily }}>
+      <ChatWindow />
     </div>
   );
 }
